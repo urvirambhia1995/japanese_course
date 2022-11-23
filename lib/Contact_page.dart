47847ca53865f7626274847c561cb52570379605
uru <urvi.rambhia@watchyourhealth.com>
2022-11-23 20:21:51 +0530

    help screen and contact screen done

--- lib/Contact_page.dart
+++ lib/Contact_page.dart
@@ -9,6 +9,18 @@ class Contactpage extends StatefulWidget {
 }
 
 class _ContactpageState extends State<Contactpage> {
+  // Initial Selected Value
+  String dropdownvalue = 'Item 1';
+
+  // List of items in our dropdown menu
+  var items = [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,27 +28,128 @@ class _ContactpageState extends State<Contactpage> {
         elevation: 0,
         backgroundColor: Colors.transparent,
         leading: Container(
-          margin: EdgeInsets.fromLTRB(10,5,2,10),
+          margin: EdgeInsets.fromLTRB(10, 5, 2, 10),
           child: Card(
-            child: Icon(Icons.arrow_back,color: Colors.black,),
+            child: Icon(
+              Icons.arrow_back,
+              color: Colors.black,
+            ),
             elevation: 5,
             shadowColor: Colors.grey.shade500,
-            shape: RoundedRectangleBorder(side: BorderSide(color: Colors.grey.shade200,width: 1),borderRadius: BorderRadius.circular(10)),
+            shape: RoundedRectangleBorder(
+                side: BorderSide(color: Colors.grey.shade200, width: 1),
+                borderRadius: BorderRadius.circular(10)),
           ),
         ),
-        title:Container(
-          margin: EdgeInsets.fromLTRB(0,10,0,12),
-          child: Text('Contact Us',style: TextStyle(color: Colors.blue.shade900,fontWeight: FontWeight.bold),), ),
+        title: Container(
+          margin: EdgeInsets.fromLTRB(0, 10, 0, 12),
+          child: Text(
+            'Contact Us',
+            style: TextStyle(
+                color: Colors.blue.shade900, fontWeight: FontWeight.bold),
+          ),
+        ),
       ),
-
-      body: Column(children: [],),
+      body: Container(
+        padding: EdgeInsets.fromLTRB(10, 50, 10, 0),
+        child: Column(
+          children: [
+            Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  'FullName',
+                  style: TextStyle(
+                      color: Colors.blue.shade900, fontWeight: FontWeight.bold),
+                )),
+            Card(
+                color: Colors.blue.shade50,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        hintText: 'Mochammad Rheza',
+                        hintStyle: TextStyle(color: Colors.black54)),
+                  ),
+                )),
+            Container(
+                alignment: Alignment.topLeft,
+                margin: EdgeInsets.only(top: 10),
+                child: Text(
+                  'Email',
+                  style: TextStyle(
+                      color: Colors.blue.shade900, fontWeight: FontWeight.bold),
+                )),
+            Card(
+                color: Colors.blue.shade50,
+                child: Padding(
+                    padding: EdgeInsets.only(left: 8),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          hintText: 'Rheza@gmail.com',
+                          hintStyle: TextStyle(color: Colors.black54)),
+                    ))),
+            Container(
+                alignment: Alignment.topLeft,
+                margin: EdgeInsets.only(top: 10),
+                child: Text(
+                  'Issues',
+                  style: TextStyle(
+                      color: Colors.blue.shade900, fontWeight: FontWeight.bold),
+                )),
+            Card(
+              color: Colors.blue.shade50,
+              child: DropdownButton(
+                value: dropdownvalue,
+                icon: Align(alignment: Alignment.topRight,child: const Icon(Icons.keyboard_arrow_down)),
+                items: items.map((String items) {
+                  return DropdownMenuItem(
+                    value: items,
+                    child: Container(width: MediaQuery.of(context).size.height/2.5,padding: EdgeInsets.only(left: 10),child: Text(items,style: TextStyle(color: Colors.black54),)),
+                  );
+                }).toList(),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    dropdownvalue = newValue!;
+                  });
+                },
+              ),
+            ),
+            Container(
+                alignment: Alignment.topLeft,
+                margin: EdgeInsets.only(top: 10),
+                child: Text(
+                  'Detail',
+                  style: TextStyle(
+                      color: Colors.blue.shade900, fontWeight: FontWeight.bold),
+                )),
+            Card(
+                color: Colors.blue.shade50,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    maxLines: 8,
+                    decoration: InputDecoration(
+                        hintText: 'lorem ipsum',
+                        hintStyle: TextStyle(color: Colors.black54)),
+                  ),
+                ))
+          ],
+        ),
+      ),
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.all(20.0),
         child: BottomAppBar(
-            elevation: 5,child: Padding(
+            elevation: 5,
+            child: Padding(
               padding: const EdgeInsets.all(10.0),
-              child: Text('Send',textAlign:TextAlign.center,style: TextStyle(color: Colors.blue.shade900,fontWeight: FontWeight.bold),),
-            ),color: Colors.yellow.shade700),
+              child: Text(
+                'Send',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.blue.shade900, fontWeight: FontWeight.bold),
+              ),
+            ),
+            color: Colors.yellow.shade700),
       ),
     );
   }
--- lib/Help.dart
+++ lib/Help.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:japanese_course/CourseFreeTrialPage.dart';
+import 'package:japanese_course/CourseRefundPolicy.dart';
+import 'package:japanese_course/ResetPasswrod.dart';
 
 import 'Contact_page.dart';
 
@@ -15,74 +18,136 @@ class _Help_pageState extends State<Help_page> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        // appBar: AppBar(
-        //   leading: Card(
-        //     elevation: 5,
-        //       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-        //       color: Colors.white,
-        //       child: Icon(Icons.arrow_back_rounded,color: Colors.black,)),
-        //   backgroundColor: Colors.blue.shade900,
-        //   title: Text('Get Help',style: TextStyle(color: Colors.white),),
-        // ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Container(
-              height: MediaQuery.of(context).size.width / 1.5,
-              width: double.infinity,
-              child: Card(
-                elevation: 2,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(50),
-                        bottomRight: Radius.circular(50))),
-                color: Colors.blue.shade900,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    SizedBox(
-                      height: 50,width: 50,
-                      child: Card(
-                        color: Colors.white,
-                        elevation: 5,
-                        margin: EdgeInsets.fromLTRB(10,8,0,190),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(5)),
-                        child: Icon(
-                          Icons.arrow_back,
-                          color: Colors.blue.shade900,
-                          size: 20,
+              SizedBox(
+                 height: MediaQuery.of(context).size.width *0.65,
+                 width: double.infinity,
+                child: Card(
+                  elevation: 2,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(50),
+                          bottomRight: Radius.circular(50))),
+                  color: Colors.blue.shade900,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Flexible(
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Container(
+                                height: 50,
+                                width: 50,
+                                child: Expanded(
+                                  child: Card(
+                                    color: Colors.white,
+                                    elevation: 5,
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(5)),
+                                    child: Icon(
+                                      Icons.arrow_back,
+                                      color: Colors.blue.shade900,
+                                      size: 20,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                  margin: EdgeInsets.fromLTRB(10, 10, 0, 0),
+                                  child: Text(
+                                    'Get Help',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18),
+                                  ))
+                            ],
+                          ),
                         ),
                       ),
+                      Padding(
+                        padding: const EdgeInsets.all(3.0),
+                        child: Text('We are here to help.',textAlign: TextAlign.center,style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold),),
+                      ),
+                      Container(
+                          height: MediaQuery.of(context).size.height*0.1,width: MediaQuery.of(context).size.width*0.6,
+                          padding: EdgeInsets.all(15),
+                          child: TextField(obscureText: true,decoration: InputDecoration(filled:true,fillColor:Colors.white,border: OutlineInputBorder(borderRadius: BorderRadius.circular(20),),hintText: 'Search',prefixIcon: Icon(Icons.search_sharp),),))
+                    ],
+                  ),
+                ),
+              ),
+
+            Flexible(
+              child: Container(
+                child: Card(
+                  elevation: 5,
+                  margin: EdgeInsets.all(10),
+                  shape: RoundedRectangleBorder(
+                      side: BorderSide(width: 1, color: Colors.grey.shade200),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => CourseFreeTrialPage()));
+                    },
+                    child: Text(
+                      'Course Free Trail',
+                      style: TextStyle(
+                          color: Colors.blue.shade900, fontWeight: FontWeight.bold),
                     ),
-                    Container(
-                        margin: EdgeInsets.fromLTRB(10,15,0,0),
-                        child: Text('Get Help',textAlign: TextAlign.center,style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 18),))
-                  ],
+                  ),
                 ),
               ),
             ),
             Card(
               elevation: 5,
               margin: EdgeInsets.all(10),
-              shape: RoundedRectangleBorder(side: BorderSide(width: 1,color: Colors.grey.shade200),borderRadius: BorderRadius.circular(10)),
-              child: TextButton( onPressed: () {  },
-                child: Text('Course Free Trail',style: TextStyle(color: Colors.blue.shade900,fontWeight: FontWeight.bold),),),
+              shape: RoundedRectangleBorder(
+                  side: BorderSide(width: 1, color: Colors.grey.shade200),
+                  borderRadius: BorderRadius.circular(10)),
+              child: TextButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => CourseRefundPloicy()));
+                },
+                child: Text(
+                  'Course Refund Policy',
+                  style: TextStyle(
+                      color: Colors.blue.shade900, fontWeight: FontWeight.bold),
+                ),
+              ),
             ),
             Card(
               elevation: 5,
               margin: EdgeInsets.all(10),
-              shape: RoundedRectangleBorder(side: BorderSide(width: 1,color: Colors.grey.shade200),borderRadius: BorderRadius.circular(10)),
-              child: TextButton( onPressed: () {  },
-                child: Text('Course Refund Policy',style: TextStyle(color: Colors.blue.shade900,fontWeight: FontWeight.bold),),),
-            ),
-            Card(
-              elevation: 5,
-              margin: EdgeInsets.all(10),
-              shape: RoundedRectangleBorder(side: BorderSide(width: 1,color: Colors.grey.shade200),borderRadius: BorderRadius.circular(10)),
-              child: TextButton( onPressed: () {  },
-                child: Text('Reset Password',style: TextStyle(color: Colors.blue.shade900,fontWeight: FontWeight.bold),),),
+              shape: RoundedRectangleBorder(
+                  side: BorderSide(width: 1, color: Colors.grey.shade200),
+                  borderRadius: BorderRadius.circular(10)),
+              child: TextButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => ResetPassword()));
+                },
+                child: Text(
+                  'Reset Password',
+                  style: TextStyle(
+                      color: Colors.blue.shade900, fontWeight: FontWeight.bold),
+                ),
+              ),
             )
           ],
         ),
@@ -90,15 +155,26 @@ class _Help_pageState extends State<Help_page> {
           elevation: 0,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
-           // crossAxisAlignment: CrossAxisAlignment.center,
+            // crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text('Have more questions?',style: TextStyle(color: Colors.grey.shade500,fontSize: 15,fontWeight: FontWeight.bold),),
-              TextButton(onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => Contactpage()));
-
-              },clipBehavior: Clip.hardEdge,
-              child: Text('Contact Us!',style: TextStyle(color: Colors.blue.shade900),),),
+              Text(
+                'Have more questions?',
+                style: TextStyle(
+                    color: Colors.grey.shade500,
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => Contactpage()));
+                },
+                clipBehavior: Clip.hardEdge,
+                child: Text(
+                  'Contact Us!',
+                  style: TextStyle(color: Colors.blue.shade900),
+                ),
+              ),
             ],
           ),
         ),
